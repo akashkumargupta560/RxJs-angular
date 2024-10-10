c7f8e2ae83d7517bcc7a6bae9b62d52a33faff67
chehak-gupta <chehakguptaameotech@gmail.com>
2024-10-10 15:52:28 +0530

    custom functionality work

--- src/app/pages/observable/components/custom-observable/custom-observable.component.ts
+++ src/app/pages/observable/components/custom-observable/custom-observable.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { RxjsService } from 'src/app/services/rxjs.service';
 
 @Component({
@@ -7,9 +7,13 @@ import { RxjsService } from 'src/app/services/rxjs.service';
   templateUrl: './custom-observable.component.html',
   styleUrls: ['./custom-observable.component.scss']
 })
-export class CustomObservableComponent {
+export class CustomObservableComponent implements OnInit, OnDestroy{
 
   techStatus:any;
+  techStatus2:any;
+  sub2!:Subscription;
+  names:any;
+  nameStatus:any;
   constructor( private _rxjsSrv:RxjsService){}
 
   ngOnInit():void{
@@ -26,7 +30,7 @@ export class CustomObservableComponent {
 
       setTimeout(()=>{
         observer.next('View.js');
-        
+        observer.complete();
       },3000);
       setTimeout(()=>{
         observer.next('Laravel');
@@ -34,19 +38,84 @@ export class CustomObservableComponent {
       },4000);
       setTimeout(()=>{
         observer.next('Express js');
-        observer.complete();
+        // observer.complete();
       },5000);
     });
 
     cusObj1.subscribe((res:any) =>{
-      console.log(res)
+      //console.log(res)
       this._rxjsSrv.printData(res,'elContainer')
     },
     (err:any) =>{
-      this.techStatus = "error"
+      this.techStatus = "error";
     },
     ()=>{
        this.techStatus = "completed";
-    })
+    });
+
+    //Example- 02 (Custom Interval);
+    let arr2 = ['Angular','javascript','php','reactjs','Java','Nodejs']
+     const custObjs2 = Observable.create((observer:any) =>{
+      let count = 0;
+      setInterval(()=>{
+        // observer.next('data emit' + count);
+        observer.next(arr2[count]);
+        if(count >= 3){
+          observer.error('Error Emit')
+        }
+        if(count >= 5){
+          observer.complete()
+        }
+        count++;
+      },1000)
+     })
+
+     this.sub2 = custObjs2.subscribe((res:any) =>{
+      //console.log(res);
+      this._rxjsSrv.printData(res,"elContaine2")
+     },
+     (err:any) =>{
+   
+      this.techStatus2 = "error";
+    },
+    ()=>{
+       
+       this.techStatus2 = "completed";
+    });
+    
+
+    ///Example - 03 (Random Names)
+
+    let arr3 = ['Anup','Shekhar','Sharma','Monu','Ravi','Jonson']
+    const custObj3 =Observable.create((observer:any) =>{
+      let count = 0;
+      setInterval(()=>{
+        // observer.next('data emit' + count);
+        observer.next(arr3[count]);
+        if(count >= 3){
+          observer.error('Error Emit')
+        }
+        if(count >= 5){
+          observer.complete()
+        }
+        count++;
+      },1000)
+    });
+    custObj3.subscribe((res:any) =>{
+      console.log('sdf',res);
+      this.names = res
+    },
+    (err:any) =>{
+   
+      this.nameStatus= "error";
+    },
+    ()=>{
+       
+       this.nameStatus = "completed";
+    }
+  ) 
+  }
+  ngOnDestroy(): void {
+    this.sub2.unsubscribe();
   }
 }
